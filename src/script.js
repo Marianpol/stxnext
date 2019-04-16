2dbc3e89edfdd508b918236ada53916c9c76206b
Marianpol <marcin.musial20@gmail.com>
2019-04-16 23:13:06 +0200

    Load more results at the end of page

--- src/script.js
+++ src/script.js
@@ -1,33 +1,37 @@
 const userInput = document.querySelector('input');
 const searchButton = document.querySelector('button');
 const searchResults = document.querySelector('div');
-
-
+let tmpUserInput= '';
+let searchTitle = '';
+let i = 0;
+let totalItems = 0;
 
 searchButton.addEventListener('click', function (event){
-    let searchTitle = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput.value}`;
+    tmpUserInput = userInput.value;
+    userInput.value = "";
+    searchTitle = `https://www.googleapis.com/books/v1/volumes?q=intitle:${tmpUserInput}`;
     event.preventDefault();
-    searchResults.innerHTML = drawTable(getUrl(searchTitle));
-    
+    searchResults.innerHTML = drawTable(getUrl(searchTitle)); 
 });
 
 function getUrl(url){
     const request = new XMLHttpRequest();
     request.open('GET', url, false);
     request.send();
+    console.log(JSON.parse(request.responseText));
     return JSON.parse(request.responseText);
     
 }
 
 function drawTable(data){
     const books = data.items;
+    totalItems = data.totalItems;
     let table = '<table border="1"';
 	for (let book of books){
 		table += `<tr><td><img src="${isAvailable(book.volumeInfo.imageLinks, 'thumbnail')}" alt="No Preview"></td>
 		          <td>${book.volumeInfo.title}</td>
                   <td>${isAvailable(book.volumeInfo.description, '' , "No description")}</td></tr> `;   
     }
-    table += '</table>';
     return table;
 }
 function isAvailable(checkElem, addElem, text){
@@ -40,4 +44,18 @@ function isAvailable(checkElem, addElem, text){
     else {
         return text;
     }
+}
+
+window.onscroll = function(){
+    let offset = document.documentElement.scrollTop + window.innerHeight;
+    let height = document.documentElement.offsetHeight;
+    let startIndex = 1+10*i;
+    searchTitle = `https://www.googleapis.com/books/v1/volumes?q=intitle:${tmpUserInput}&startIndex=${startIndex}&maxResults=10`;
+
+    if(startIndex < totalItems){
+        if (offset >= height) {
+            searchResults.innerHTML += drawTable(getUrl(searchTitle));
+            i++; 
+      }}
+    
 }
